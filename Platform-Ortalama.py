03b9fb7fb49bd4a8cb561e399c6a56a7c743a90e
ali21devrim <devrimali@sabanciuniv.edu>
2025-01-10 20:11:27 +0300

    Update Platform-Ortalama.py

--- Platform-Ortalama.py
+++ Platform-Ortalama.py
@@ -1,26 +1,28 @@
+**This python code was written to show which application I use for how many minutes per day on average. The result is depicted as bar graphs.**
+                                                                                                                              
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Excel dosyasını yükleme
+# Upload Excel files
 file_path = r"C:\Users\alide\OneDrive - sabanciuniv.edu\Masaüstü\CS210 Project\Ekran Süresi Dataları.xlsx"
 social_media_data = pd.read_excel(file_path)
 
-# Platformların günlük ortalama sürelerini hesaplama
+# Calculating daily average times of platforms
 platforms = ["TikTok", "Instagram", "Twitter (X)", "Youtube"]
 average_durations = social_media_data[platforms].mean()
 
-# Grafik oluşturma
+# Creating a chart
 plt.figure(figsize=(10, 6))
 average_durations.plot(kind='bar', color=['black', 'red', 'blue', 'gray'], edgecolor='black')
 
-# Grafik etiketleri ve başlık
+# Chart labels and title
 plt.title("Average Daily Social Media Usage by Platform")
 plt.xlabel("Social Media Platform")
 plt.ylabel("Average Usage Time (Minutes)")
 plt.xticks(rotation=0)  # X ekseni etiketlerini yatay tut
 
-# Grafik düzenlemesi
+# Graphics editing
 plt.tight_layout()
 
-# Grafiği gösterme
+# Show the graph
 plt.show()
